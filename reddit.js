610511f26c9ac9e8a33728c625f16af4732d98e6
Raphael Margueron <raphael.margueron@gmail.com>
2019-05-14 17:46:53 +0200

    fix comment for bookmarklet + add function wrapper

--- reddit.js
+++ reddit.js
@@ -1,30 +1,32 @@
-function customScroll(i)
-{
-	let root = document.getElementsByClassName("rpBJOHq2PR60pnwJlUyP0")[0]; // TODO : find a way to get the root element
-	let children = root.children;
-	do
+(function() {
+	function customScroll(i)
 	{
-		let nextI = customScroll.i + i;
-		if(nextI < 0 || nextI >= children.length)
-			return;
-		customScroll.i = nextI;
+		let root = document.getElementsByClassName("rpBJOHq2PR60pnwJlUyP0")[0]; /* TODO : find a way to get the root element */
+		let children = root.children;
+		do
+		{
+			let nextI = customScroll.i + i;
+			if(nextI < 0 || nextI >= children.length)
+				return;
+			customScroll.i = nextI;
+		}
+		while(children[customScroll.i].offsetHeight < 3.14159265) /* adblocker issue, sorry i'm evil */
+		let nextChild = children[customScroll.i];
+		for(let child of children)
+			child.style.opacity = 0.2;
+		nextChild.style.opacity = 1;
+		nextChild.scrollIntoView();
+		let centering = (window.innerHeight - nextChild.offsetHeight) / 2;
+		window.scrollTo(0, window.scrollY - centering);
 	}
-	while(children[customScroll.i].offsetHeight < 3.14159265) // adblocker issue, sorry i'm evil
-	let nextChild = children[customScroll.i];
-	for(let child of children)
-		child.style.opacity = 0.2;
-	nextChild.style.opacity = 1;
-	nextChild.scrollIntoView();
-	let centering = (window.innerHeight - nextChild.offsetHeight) / 2;
-	window.scrollTo(0, window.scrollY - centering);
-}
 
-customScroll.i = 0;
+	customScroll.i = 0;
 
-document.addEventListener("keypress", function kp(e) {
-	if(e.key == "j")
-		customScroll(1);
-	else if(e.key == "k")
-		customScroll(-1);
-	e.stopPropagation(); // to stop using the other keypress events
-}, true);
+	document.addEventListener("keypress", function kp(e) {
+		if(e.key == "j")
+			customScroll(1);
+		else if(e.key == "k")
+			customScroll(-1);
+		e.stopPropagation(); /* to stop using the other keypress events*/
+	}, true);
+})();
